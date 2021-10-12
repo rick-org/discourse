047645dd35828ecae9acfc5e18ed09f21b2b0c72
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-10-12 20:44:16 +0200

    Use UsernameChanger

--- app/services/username_overrider.rb
+++ app/services/username_overrider.rb
@@ -3,11 +3,10 @@
 class UsernameOverrider
   def self.override(user, new_username)
     if user.username_lower == User.normalize_username(new_username)
-      user.username = new_username # there may be a change of case
+      UsernameChanger.change(user, new_username, user)
       true
     elsif user.username != UserNameSuggester.fix_username(new_username)
-      suggested_username = UserNameSuggester.suggest(new_username)
-      UsernameChanger.change(user, suggested_username, user)
+      user.username = UserNameSuggester.suggest(new_username)
       true
     else
       false
