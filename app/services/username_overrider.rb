41a59d7e913997475a82c84cc384d99347e1a7f3
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-10-12 20:42:25 +0200

    Use username_lower and User.normilize_username

--- app/services/username_overrider.rb
+++ app/services/username_overrider.rb
@@ -2,11 +2,12 @@
 
 class UsernameOverrider
   def self.override(user, new_username)
-    if user.username.downcase == new_username.downcase
+    if user.username_lower == User.normalize_username(new_username)
       user.username = new_username # there may be a change of case
       true
     elsif user.username != UserNameSuggester.fix_username(new_username)
-      user.username = UserNameSuggester.suggest(new_username)
+      suggested_username = UserNameSuggester.suggest(new_username)
+      UsernameChanger.change(user, suggested_username, user)
       true
     else
       false
